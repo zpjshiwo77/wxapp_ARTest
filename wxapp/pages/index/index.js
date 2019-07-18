de03fae6da9ac40139cda9cd81222ad1b95179a4
page <page@beats.net.cn>
2019-07-18 13:40:32 +0800

    修改，优化，识别彼邑logo

--- wxapp/pages/index/index.js
+++ wxapp/pages/index/index.js
@@ -1,168 +1,55 @@
-const vs = `
-  attribute vec3 aPos;
-  attribute vec2 aVertexTextureCoord;
-  varying highp vec2 vTextureCoord;
-
-  void main(void){
-    gl_Position = vec4(aPos, 1);
-    vTextureCoord = aVertexTextureCoord;
-  }
-`
-
-const fs = `
-  varying highp vec2 vTextureCoord;
-  uniform sampler2D uSampler;
-
-  void main(void) {
-    gl_FragColor = texture2D(uSampler, vTextureCoord);
-  }
-`
-
-const vertex = [
-  -1, -1, 0.0,
-  1, -1, 0.0,
-  1, 1, 0.0,
-  -1, 1, 0.0
-]
-
-const vertexIndice = [
-  0, 1, 2,
-  0, 2, 3
-]
-
-const texCoords = [
-  0.0, 0.0,
-  1.0, 0.0,
-  1.0, 1.0,
-  0.0, 1.0
-]
-
-function createShader(gl, src, type) {
-  const shader = gl.createShader(type)
-  gl.shaderSource(shader, src)
-  gl.compileShader(shader)
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error('Error compiling shader: ' + gl.getShaderInfoLog(shader))
-  }
-  return shader
-}
-
-const buffers = {}
-
-function createRenderer(canvas, width, height) {
-  const gl = canvas.getContext("webgl")
-  if (!gl) {
-    console.error('Unable to get webgl context.')
-    return
-  }
-
-  const info = wx.getSystemInfoSync()
-  gl.canvas.width = info.pixelRatio * width
-  gl.canvas.height = info.pixelRatio * height
-  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
-
-  const vertexShader = createShader(gl, vs, gl.VERTEX_SHADER)
-  const fragmentShader = createShader(gl, fs, gl.FRAGMENT_SHADER)
-
-  const program = gl.createProgram()
-  gl.attachShader(program, vertexShader)
-  gl.attachShader(program, fragmentShader)
-  gl.linkProgram(program)
-
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    console.error('Unable to initialize the shader program.')
-    return
-  }
-
-  gl.useProgram(program)
-
-  const texture = gl.createTexture()
-  gl.activeTexture(gl.TEXTURE0)
-  gl.bindTexture(gl.TEXTURE_2D, texture)
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-  gl.bindTexture(gl.TEXTURE_2D, null)
-
-  buffers.vertexBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.vertexBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertex), gl.STATIC_DRAW)
-
-  buffers.vertexIndiceBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.vertexIndiceBuffer)
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vertexIndice), gl.STATIC_DRAW)
-
-  const aVertexPosition = gl.getAttribLocation(program, 'aPos')
-  gl.vertexAttribPointer(aVertexPosition, 3, gl.FLOAT, false, 0, 0)
-  gl.enableVertexAttribArray(aVertexPosition)
-
-  buffers.trianglesTexCoordBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffers.trianglesTexCoordBuffer)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW)
-
-  const vertexTexCoordAttribute = gl.getAttribLocation(program, "aVertexTextureCoord")
-  gl.enableVertexAttribArray(vertexTexCoordAttribute)
-  gl.vertexAttribPointer(vertexTexCoordAttribute, 2, gl.FLOAT, false, 0, 0)
-
-  const samplerUniform = gl.getUniformLocation(program, 'uSampler')
-  gl.uniform1i(samplerUniform, 0)
-
-  return (arrayBuffer, width, height) => {
-    gl.bindTexture(gl.TEXTURE_2D, texture)
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, arrayBuffer)
-    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
-  }
-}
-
-
-// const upng = require('../../utils/upng.js');
-// console.log(upng.encode)
-
 let discernFlag = true;
+let GyroscopeFlag = true;
 let successFlag = false;
+let timer = null;
+let GyrData = null;
 
 Page({
   data: {
-    width: 750,
-    height: 963,
+    width: 0,
+    height: 0,
     show: false,
     word: "",
     word2: "",
     base64: ""
   },
   onReady: function () {
-    const selector = wx.createSelectorQuery()
-    selector.select('#webgl')
-      .node(this.init.bind(this))
-      .exec()
+    var info = wx.getSystemInfoSync();
+    var h = 750 / info.windowWidth * info.windowHeight;
+    this.setData({
+      height:h
+    });
+    this.startGyr();
+  },
+  
+  /**
+   * 开启陀螺仪
+   */
+  startGyr(){
+    var that = this;
+    wx.startGyroscope();
+    wx.onGyroscopeChange(function(e){
+      if(GyroscopeFlag){
+        GyroscopeFlag = false;
+        setTimeout(function(){
+          GyroscopeFlag = true;
+        },1000);
+        if(GyrData){
+          console.log(Math.abs(GyrData.x - e.x) ,Math.abs(GyrData.y - e.y) ,Math.abs(GyrData.z - e.z) )
+          if(Math.abs(GyrData.x - e.x) < 0.1 && Math.abs(GyrData.y - e.y) < 0.1 && Math.abs(GyrData.z - e.z) < 0.1){
+            that.takePhoto();
+          }
+          GyrData = e;
+        }
+        else GyrData = e;
+      }
+    });
   },
 
-  init(res) {
-    const canvas = res.node
-    const context = wx.createCameraContext()
-    const render = createRenderer(canvas, this.data.width, this.data.height)
-
-    if (!render || typeof render !== 'function') return
-
-    const listener = context.onCameraFrame((frame) => {
-      var img = new Uint8Array(frame.data);
-      render(img, frame.width, frame.height)
-      // this.discernImg(img);
-      this.takePhoto(context);
-    })
-    listener.start()
-  },
-
-  takePhoto(camera) {
+  takePhoto() {
     if (discernFlag && !successFlag) {
       discernFlag = false;
-      setTimeout(function () {
-        discernFlag = true;
-      }, 5000);
-
+      const camera = wx.createCameraContext()
       camera.takePhoto({
         success: (res) => {
           this.uploadImg(res);
@@ -171,26 +58,50 @@ Page({
     }
   },
 
+  againTry() {
+    this.setData({
+      show: false
+    });
+    successFlag = false;
+  },
+
   uploadImg(res) {
+    var that = this;
     wx.uploadFile({
-      url: "https://wxapp_test.beats-digital.com/ar/api.php?action=BaiduAI-recognizeDinnerFromFile",
+      url: "https://wxapptest.beats-digital.com/ar/api.php?action=BaiduAI-recognize",
       filePath: res.tempImagePath,
       name: "img",
       success: (res) => {
+        discernFlag = true;
         var data = JSON.parse(res.data);
-        if(data.data[0].name != "非菜"){
-          this.setData({
-            show:true
-          })
-        }
-        console.log(data)
+        console.log(data.data);
+        that.juaged(data.data);
       },
       fail: (res) => {
+        discernFlag = true;
         console.log(res)
       }
     });
   },
 
+  /**
+   * 判断返回的结果是否有logo 信赖值>80%及有
+   * @param {*} res 
+   */
+  juaged(res) {
+    for (let i = 0; i < res.length; i++) {
+      const ele = res[i];
+      if (ele.score > 0.8) {
+        successFlag = true;
+        this.setData({
+          show: true
+        });
+        return;
+      }
+    }
+  },
+
+  /********* 下面是像素点转base64的方法，暂时是错误的 **************/
   discernImg(data) {
     if (discernFlag && !successFlag) {
       discernFlag = false;
@@ -205,7 +116,7 @@ Page({
 
   sendImg(img) {
     wx.request({
-      url: "https://wxapp_test.beats-digital.com/ar/api.php?action=BaiduAI-recognizeDinner",
+      url: "https://wxapp_test.beats-digital.com/ar/api.php?action=BaiduAI-recognize",
       data: { img: "data:image/jpeg;base64," + img },
       method: "POST",
       header: {
